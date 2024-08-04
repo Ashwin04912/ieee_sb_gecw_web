bb547e5be987ad4b556db895d52023c9dc37b00a
Ashwin04912 <achuashwin049@gmail.com>
2024-08-05 02:28:55 +0530

    bg-image added

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,4 @@
 @extends('layouts.app')
+@section('content')
+    <h1>fadjfkljdkkjl</h1>
+@endsection
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
-<section class="bsb-hero-5 px-3 bsb-overlay" style="background-image: url('https://e1.pxfuel.com/desktop-wallpaper/281/572/desktop-wallpaper-10-chill-horizon-anime-thumbnail.jpg');">
-     
+<section class="bsb-hero-5 bsb-overlay img-fluid " style="background-image: url('{{ asset('assets/dummy-bg.jpg') }}'); height :104vh">
+
 <nav class="navbar navbar-margin-top navbar-expand-lg navbar-light hi">
-    <div class="container-fluid">
+    <div class="container-fluid fixed-top">
         <img src="{{ asset('assets/logo.png') }}" height="110px" width="110px" alt="" srcset="">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,35 +9,35 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item {{ Route::currentRouteName() == 'home' ? 'selected' : '' }}">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link active text-light" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item {{ Route::currentRouteName() == 'about.us' ? 'selected' : '' }}">
-                    <a class="nav-link" href="{{ route('about.us') }}">About Us</a>
+                    <a class="nav-link text-light" href="{{ route('about.us') }}">About Us</a>
                 </li>
                 <li class="nav-item {{ Route::currentRouteName() == 'events' ? 'selected' : '' }}">
-                    <a class="nav-link" href="{{ route('events') }}">Events</a>
+                    <a class="nav-link text-light" href="{{ route('events') }}">Events</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Execom
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Achievements</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item text-light" href="#">Achievements</a></li>
+                        <li><a class="dropdown-item text-light" href="#">Another action</a></li>
+                        <li><a class="dropdown-item text-light" href="#">Something else here</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Achievements</a>
+                    <a class="nav-link text-light" href="#">Achievements</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Societies
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Computer Society</a></li>
-                        <li><a class="dropdown-item" href="#">Women In Engineering</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item  text-light" href="#">Computer Society</a></li>
+                        <li><a class="dropdown-item text-light" href="#">Women In Engineering</a></li>
+                        <li><a class="dropdown-item text-light" href="#">Something else here</a></li>
                     </ul>
                 </li>
             </ul>
@@ -54,7 +54,7 @@
             <span class="col-12 col-sm-10 col-md-8 col-xxl-7">There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.</span>
           </p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn bsb-btn-2xl btn-outline-light">Free Consultation</button>
+            <button type="button" class="btn bsb-btn-2xl btn-outline-light">Get Started</button>
           </div>
         </div>
       </div>
