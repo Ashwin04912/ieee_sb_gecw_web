c7e0412195774a0fc62c2b3c6d10e14619899d95
Ashwin04912 <achuashwin049@gmail.com>
2024-08-05 01:11:38 +0530

    env added

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class = main>
-
+<section class="bsb-hero-5 px-3 bsb-overlay" style="background-image: url('https://e1.pxfuel.com/desktop-wallpaper/281/572/desktop-wallpaper-10-chill-horizon-anime-thumbnail.jpg');">
+    <div class="container">
+      <div class="row justify-content-md-center align-items-center">
+        <div class="col-12 col-md-11 col-lg-9 col-xl-8 col-xxl-7">
+          <h2 class="display-1 text-white text-center fw-bold mb-4">Dream of Success</h2>
+          <p class="lead text-white text-center mb-5 d-flex justify-content-sm-center">
+            <span class="col-12 col-sm-10 col-md-8 col-xxl-7">There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.</span>
+          </p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" class="btn bsb-btn-2xl btn-outline-light">Free Consultation</button>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
+  </section>
 @endsection
-
-<style>
-    .main{
-        height: 200px;
-    }
-</style>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,13 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/heroes/hero-5/assets/css/hero-5.css">
 </head>
 
 <body>
-    <div class="container-scroller">
+    <div class="container-scroller mt-0 p-0">
         @include('layouts.navigation')
        
         <div class="container-fluid page-body-wrapper" >
