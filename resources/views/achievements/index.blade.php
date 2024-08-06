b457f50f9552d5801c933e0a40248b9f95408909
Nihan Cj <nihancj.com@gmail.com>
2024-08-06 22:40:43 +0530

    achievements: add title

--- resources/views/achievements/index.blade.php
+++ resources/views/achievements/index.blade.php
@@ -1,1 +1,29 @@
 @include('layouts.app')
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IEEE SB GECW - Achievements</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+    <div class="container pt-5 my_container">
+        <h2 align="center">ACHIEVEMENTS</h2>
+        <div class=" d-flex justify-content-center">
+            <hr style="width: 300px">
+        </div>
+        <br>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
