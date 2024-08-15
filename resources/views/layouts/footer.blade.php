b77056309b88de07bb5ea7334ace9d2ac5588ffe
Nihan Cj <nihancj.com@gmail.com>
2024-08-15 21:28:15 +0530

    Add new footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,76 @@
+<footer class="text-white text-center text-lg-start" style="background-color: #23242a;">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!--Grid row-->
+        <div class="row mt-4">
+            <!--Grid column-->
+            <div class="col-lg-5 col-md-12 mb-4 mb-md-0">
+                <h5 class="text mb-4">IEEE SB GECW</h5>
+                <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+                    voluptatum deleniti atque corrupti.
+                </p>
+            </div>
+            <!--Grid column-->
 
-  <footer class="footer ">
-      <div class="footer-inner-wraper">
-          <div class="d-sm-flex py-2", style="z-index:aut">
-              <span class="text-muted w-100 text-center ">Copyright © <a href="#" target="_blank">TeamCodia</a> 2024</span>
-              
-          </div>
-      </div>
-  </footer>
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text mb-4">Contact Us</h5>
+                <ul class="fa-ul" style="margin-left: 1.65em;">
+                    <li class="mb-3">
+                        <span class="fa-li"><i class="fas fa-location-dot"></i></span>
+                        <span class="ms-2">
+                            <a href="https://maps.app.goo.gl/nicoo6Cz1wppTm2Z9"
+                                style="color:white;text-decoration:none">Thalapuzha, NY 10012, US</a>
+                        </span>
+                    </li>
+                    <li class="mb-3">
+                        <span class="fa-li"><i class="fas fa-envelope"></i></span>
+                        <span class="ms-2">ieeesb@gecwyd.ac.in</span>
+                    </li>
+                    <li class="mb-3">
+                        <span class="fa-li"><i class="fas fa-phone"></i></span>
+                        <span class="ms-2">+ 01 234 567 88</span>
+                    </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text mb-4">Quick Links</h5>
+                <ul class="fa-ul" style="margin-left: 1.65em;">
+                    <li class="mb-3">
+                        <span class="fa-li"><i>-</i></span>
+                        <span class="ms-2">
+                            <a href="" style="color:white;text-decoration:none">Gallery</a>
+                        </span>
+                    </li>
+                    <li class="mb-3">
+                        <span class="fa-li"><i>-</i></span>
+                        <span class="ms-2">
+                            <a href="" style="color:white;text-decoration:none">Events</a>
+                        </span>
+                    </li>
+                    <li class="mb-3">
+                        <span class="fa-li"><i>-</i></span>
+                        <span class="ms-2">
+                            <a href="" style="color:white;text-decoration:none">About Us</a>
+                        </span>
+                    </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+        </div>
+        <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        Designed and Developed by:
+        <a class="text-white" href="https://github.com/Ashwin04912/ieee_sb_gecw_web" style="text-decoration:none">
+            Team Codia
+        </a>
+    </div>
+</footer>
