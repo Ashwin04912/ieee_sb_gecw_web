2f169d6d21768f3b7dd257a11ba2c88e3bcb7999
Ashwin04912 <achuashwin049@gmail.com>
2024-08-24 18:00:32 +0530

    hello

--- public/assets/js/navigation.js
+++ public/assets/js/navigation.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(window).scrollTop() >= 150) {
         $('#ScrollJq').css('background', 'black');
     } else {
@@ -6,20 +6,20 @@ $(window).scroll(function() {
     }
 });
 
-$(document).ready(function() {
-var currentRoute = $('body').data('route-name');
-if (currentRoute === 'home') {
-$('#navbar').css({
-    'background': 'transparent'
-});
-} else {
-$('#navbar').css({
-    'background': 'black',
-    'opacity': '1'
-});
-}   
+$(document).ready(function () {
+    var currentRoute = $('body').data('route-name');
+    if (currentRoute === 'home') {
+        $('#navbar').css({
+            'background': 'transparent'
+        });
+    } else {
+        $('#navbar').css({
+            'background': 'black',
+            'opacity': '1'
+        });
+    }
 
-    $('a[href^="#"]').on('click', function(event) {
+    $('a[href^="#"]').on('click', function (event) {
         var target = $(this.getAttribute('href'));
         if (target.length) {
             event.preventDefault();
